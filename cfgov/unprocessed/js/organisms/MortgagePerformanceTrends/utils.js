464928e1a284108e2256453d725b1f118697a293
Chris Contolini <chris@contolini.com>
2017-08-31 23:08:24 -0400

    Remove xdr and console groups

--- cfgov/unprocessed/js/organisms/MortgagePerformanceTrends/utils.js
+++ cfgov/unprocessed/js/organisms/MortgagePerformanceTrends/utils.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var ajax = require( 'xdr' );
-
 var COUNTIES_URL = 'https://s3.amazonaws.com/files.consumerfinance.gov/data/mortgage-performance/meta/state_county_dropdown.json';
 var METROS_URL = 'https://s3.amazonaws.com/files.consumerfinance.gov/data/mortgage-performance/meta/state_msa_dropdown.json';
 var counties;
@@ -64,18 +62,6 @@ var utils = {
     if ( !window.MP_DEBUG ) {
       return next( action );
     }
-    console.groupCollapsed( action.type );
-    console.group( 'action:' );
-    console.log( JSON.stringify( action, '', '\t' ) );
-    console.groupEnd();
-    console.groupCollapsed( 'previous state:' );
-    console.log( JSON.stringify( store.getState(), '', '\t' ) );
-    console.groupEnd();
-    var result = next( action ); // eslint-disable-line
-    console.groupCollapsed( 'state:' );
-    console.log( JSON.stringify( store.getState(), '', '\t' ) );
-    console.groupEnd();
-    console.groupEnd();
     return result;
   }
 };
